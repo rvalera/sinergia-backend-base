2f9906891260b35e748a42df1834c304d452d47d
Ramon Valera <ramon.valera@gmail.com>
2020-02-18 19:45:06 -0400

    bug -  CORS Configuratio for Flask App

--- app/__init__.py
+++ app/__init__.py
@@ -19,7 +19,7 @@ def create_app(config_type='dev'):
     
     app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
     
-    CORS(app.wsgi_app, resources={r'/*': {'origins': '*'}})        
+    CORS(app, resources={r'/*': {'origins': '*'}})        
     
     app.config.from_object(config[config_type])
 
