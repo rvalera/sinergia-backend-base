012556eb49103328260360d3da1e1ddfa25cd3d2
Ramon Valera <ramon.valera@gmail.com>
2020-02-18 20:59:47 -0400

    doc - modify configuration files

--- app/__init__.py
+++ app/__init__.py
@@ -31,6 +31,6 @@ def create_app(config_type='dev'):
     from .v1 import v1_blueprint
     app.register_blueprint(v1_blueprint, url_prefix='/api/v1')      
 
-#     CORS(app, resources={r'/*': {'origins': '*'}})        
+    CORS(app, resources={r'/*': {'origins': '*'}})        
 
     return app
